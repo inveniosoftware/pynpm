49cd4a45fce0e0f780d23505fd79c33bd34a1e95
Georgios Lignos <glignos93@gmail.com>
2020-05-06 12:10:56 +0200

    installation: remove Python 2 and 3.5 support
    * Closes #7

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,10 @@
 Changes
 =======
 
+Version 0.1.2 (released TBD)
+
+- Deprecated Python versions lower than 3.6.0. Now supporting 3.6.0 and 3.7.0.
+
 Version 0.1.1 (released 2017-05-16)
 
 - Fix problem with testing click CLI output.
--- setup.py
+++ setup.py
@@ -79,10 +79,9 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Development Status :: 1 - Planning',
     ],
 )
