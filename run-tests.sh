c64ed91d89552568057ea5db21a3dd33c10e7c30
Antonio Vivace <avivace4@gmail.com>
2020-12-08 13:48:59 +0100

    ci: migrate from Travis CI to GitHub Actions

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 #
-# This file is part of PyNPM
-# Copyright (C) 2017 CERN.
+# This file is part of Invenio.
+# Copyright (C) 2015-2020 CERN.
 #
-# PyNPM is free software; you can redistribute it and/or modify
-# it under the terms of the Revised BSD License; see LICENSE file for
-# more details.
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
-pydocstyle pynpm tests docs && \
-isort -rc -c -df && \
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+python -m pytest
+python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
--- setup.py
+++ setup.py
@@ -17,14 +17,8 @@ readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
 tests_require = [
-    'check-manifest>=0.25',
-    'coverage>=4.0',
-    'isort>=4.2.2',
-    'pydocstyle>=1.0.0',
     'pytest-cache>=1.0',
-    'pytest-cov>=1.8.0',
-    'pytest-pep8>=1.0.6',
-    'pytest>=2.8.0',
+    'pytest-invenio>=1.4.0'
 ]
 
 extras_require = {
