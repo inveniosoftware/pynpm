8990fb6915caaa2c328e9ddc251e135172fa3f2d
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-02 23:02:08 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>'

--- run-tests.sh
+++ run-tests.sh
@@ -9,6 +9,5 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
-python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 python -m pytest
--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2017 CERN.
-# SPDX-FileCopyrightText: 2022 Graz University of Technology.
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""Python interface to your NPM and package.json."""
-
-from setuptools import setup
-
-setup()
